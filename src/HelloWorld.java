e11e31a511d5b2503abbabd016380e4fa59826d0
huangyl <363212682@qq.com>
2020-09-07 03:15:22 +0800

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author hyl
+ * @version v1.0: HelloWorld.java, v 0.1 2020/9/7 3:12 $
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
